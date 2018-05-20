0babbd4eb0160568f92f0ca37e7e600723520682
GaoXinwu <gaoxinwucn@foxmail.com>
2018-05-20 23:27:25 +0800

    cropper

--- js/demo.js
+++ js/demo.js
@@ -1,19 +1,43 @@
 /**
  * Created by Administrator on 2018/5/11.
  */
-var $image = $('#image');
-$image.cropper({
-    aspectRatio: 16 / 9,
-    crop: function(event) {
-        console.log(event.detail.x);
-        console.log(event.detail.y);
-        console.log(event.detail.width);
-        console.log(event.detail.height);
-        console.log(event.detail.rotate);
-        console.log(event.detail.scaleX);
-        console.log(event.detail.scaleY);
+//$('#image').cropper({
+//    aspectRatio: 1,
+//    viewMode:0,
+//    preview:".preview",
+//    crop: function (e) {
+//        //console.log(e);
+//    }
+//});
+var URL = window.URL || window.webkitURL;
+$("#uploadImg").on("change",function(){
+    var files=this.files;
+    var file,uploadedImageURL ;
+    console.log(files)
+    if(files && files.length){
+        file=files[0];
+        if (/^image\/\w+$/.test(file.type)) {
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            // 监听上传完成
+            reader.onload = function(){
+                // 拿到base64的路径
+                var imgSrc = reader.result;
+                console.log(imgSrc);
+
+                // 改变img的src属性值
+                $("#image").src=imgSrc;
+                //imgObj.src=src;
+
+        }
+    }
     }
 });
 
-// Get the Cropper.js instance after initialized
-var cropper = $image.data('cropper');
+
+var c=document.getElementById("imgDone");
+var cxt=c.getContext("2d");
+var img=new Image();
+img.src="img/cat.jpg";
+cxt.drawImage(img,0,0);
+
